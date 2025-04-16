96c70535176e8373ddcf05987effe93f7e359371
Jean Park <2245bottlejeanie@gmail.com>
2025-04-16 02:48:55 -0400

    openstreetmap api

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ import LandingPage from './pages/LandingPage';
 import CreateEventPage from './pages/CreateEventPage';
 import JoinEventPage from './pages/JoinEventPage';
 import FinalOutputPage from './pages/FinalOutputPage';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function App() {
+  const center = [51.505, -0.09];
+
   return (
+    
     <Router>
       <Routes>
         {/* 
@@ -32,6 +36,22 @@ function App() {
           Final output page: shows top 10 meeting locations
         */}
         <Route path="/final" element={<FinalOutputPage />} />
+        <Route
+          path="/map"
+          element={
+            <div style={{ width: '100%', height: '100vh' }}>
+              <MapContainer center={center} zoom={13} style={{ width: '100%', height: '100%' }}>
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={center}>
+                  <Popup>Hello from London!</Popup>
+                </Marker>
+              </MapContainer>
+            </div>
+          }
+        />
       </Routes>
     </Router>
   );
